abbb14d531d6e4f22f0885c18f78bd9a0cd2d1f7
AndyStar <andy_star@ukr.net>
2025-03-19 12:25:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
-import os
+from functools import wraps
 from typing import Callable
 
 
-def read_cache() -> dict:
-    result = dict()
-    if os.path.isfile("cache.txt"):
-        with open("cache.txt", "r") as cache_file:
-            for line in cache_file.readlines():
-                parts = line.split(":")
-                if not result.get(parts[0]):
-                    result[parts[0]] = parts[1][:-1]
+def read_cache_dict() -> dict:
+    result = cache_dict
     return result
 
 
 def cache(func: Callable) -> Callable:
     func_name = func.__name__
-    global_result = read_cache()
 
-    def inner(*args) -> list:
+    global cache_dict
+    cache_dict = dict()
+    global_result = read_cache_dict()
+
+    @wraps(func)
+    def wrapper(*args) -> list:
         one_key_list = [func_name, args]
         one_key = tuple(one_key_list)
         result_inner = global_result.get(one_key)
@@ -27,13 +25,12 @@ def cache(func: Callable) -> Callable:
             global_result[one_key] = result_inner
         else:
             print("Getting from cache")
-        save_cache(global_result)
+        save_cache_dict(global_result)
         return result_inner
 
-    return inner
+    return wrapper
 
 
-def save_cache(global_result: dict) -> None:
-    with open("cache.txt", "a") as cache_file:
-        for key, value in global_result.items():
-            cache_file.write(str(key) + ":" + str(value) + "\n")
+def save_cache_dict(global_result: dict) -> None:
+    for key, value in global_result.items():
+        cache_dict[key] = value
